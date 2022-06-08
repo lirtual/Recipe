aebc4393c05ae6ecbeb89f80f64d1f024c727854
lirtual <lirtual.gmail.com>
2022-06-08 20:58:36 +0800

    feat: 优化食谱详情图显示

--- Recipe/Views/RecipeDetailView.swift
+++ Recipe/Views/RecipeDetailView.swift
@@ -11,39 +11,43 @@ struct RecipeDetailView: View {
     var recipe:Recipe
     
     var body: some View {
-        ScrollView{
-            VStack (alignment: .leading){
-                Image(recipe.image)
-                    .resizable()
-                    .frame(height: 280)
-                    .scaledToFill()
-                    .clipped()
-                                        
+        GeometryReader { geo in
+            ScrollView{
                 VStack (alignment: .leading){
-                    Text("Ingredients")
-                        .font(.title2)
-                        .bold()
-                        .padding(.vertical , 5)
-                    ForEach(recipe.ingredients) { ingredient in
-                        Text(" ·  " + ingredient.name)
-                            .padding(.bottom , 1)
+                    ZStack {
+                        Image(recipe.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geo.size.width, height: 280)
+                            .clipped()
                     }
-                    .padding(.horizontal, 5)
                     
-                    Divider()
-                    
-                    Text("Directions")
-                        .font(.title2)
-                        .bold()
-                        .padding(.vertical , 5)
-                    ForEach(0 ..< recipe.directions.count , id: \.self) { index in
-                        Text(String(index + 1) + ". " + recipe.directions[index])
-                            .padding(.bottom , 1)
-                    }
-                    .padding(.horizontal, 5)
-                }.padding(.leading, 10)
-            }
-        }.navigationBarTitle(recipe.name)
+                    VStack (alignment: .leading){
+                        Text("Ingredients")
+                            .font(.title2)
+                            .bold()
+                            .padding(.vertical , 5)
+                        ForEach(recipe.ingredients) { ingredient in
+                            Text(" ·  " + ingredient.name)
+                                .padding(.bottom , 1)
+                        }
+                        .padding(.horizontal, 5)
+                        
+                        Divider()
+                        
+                        Text("Directions")
+                            .font(.title2)
+                            .bold()
+                            .padding(.vertical , 5)
+                        ForEach(0 ..< recipe.directions.count , id: \.self) { index in
+                            Text(String(index + 1) + ". " + recipe.directions[index])
+                                .padding(.bottom , 1)
+                        }
+                        .padding(.horizontal, 5)
+                    }.padding(.leading, 10)
+                }
+            }.navigationBarTitle(recipe.name)
+        }
     }
 }
 
