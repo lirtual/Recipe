e8fd4b2542f734e05eed72ea9cb9c5a124f1b802
lirtual <lirtual.gmail.com>
2022-06-08 22:44:56 +0800

    feat: 食谱详情页面添加份量选择功能

--- Recipe/Models/Recipe.swift
+++ Recipe/Models/Recipe.swift
@@ -41,6 +41,76 @@ class Recipe: Identifiable, Decodable{
         var num:Int?
         var unit:String?
         var denom:Int?
+        
+        func getPortion(recipeServings:Int, targetServings:Int) -> String {
+            
+            var portion = ""
+            var numerator = self.num ?? 1
+            var denominator = self.denom ?? 1
+            var wholePortions = 0
+            
+            if self.num != nil {
+                
+                // Get a single serving size by multiplying denominator by the recipe servings
+                denominator *= recipeServings
+                
+                // Get target portion by multiplying numerator by target servings
+                numerator *= targetServings
+                
+                // Reduce fraction by greatest common divisor
+                let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+                numerator /= divisor
+                denominator /= divisor
+                
+                // Get the whole portion if numerator > denominator
+                if numerator >= denominator {
+                    
+                    // Calculated whole portions
+                    wholePortions = numerator / denominator
+                    
+                    // Calculate the remainder
+                    numerator = numerator % denominator
+                    
+                    // Assign to portion string
+                    portion += String(wholePortions)
+                }
+                
+                // Express the remainder as a fraction
+                if numerator > 0 {
+                    
+                    // Assign remainder as fraction to the portion string
+                    portion += wholePortions > 0 ? " " : ""
+                    portion += "\(numerator)/\(denominator)"
+                }
+                
+            }
+            
+            if var unit = self.unit {
+                
+                // If we need to pluralize
+                if wholePortions > 1 {
+                    
+                    // Calculate appropriate suffix
+                    if unit.suffix(2) == "ch" {
+                        unit += "es"
+                    }
+                    else if unit.suffix(1) == "f" {
+                        unit = String(unit.dropLast())
+                        unit += "ves"
+                    }
+                    else {
+                        unit += "s"
+                    }
+                }
+                
+                portion += self.num == nil && self.denom == nil ? "" : " "
+                
+                return portion + unit
+            }
+            
+            return portion
+        }
+        
     }
 }
-    
+
--- Recipe/Views/RecipeDetailView.swift
+++ Recipe/Views/RecipeDetailView.swift
@@ -9,18 +9,29 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     var recipe:Recipe
+    @State var selectedIndex:Int = 2
+    let servingSelections:[Int] = [2, 4, 6, 8]
     
     var body: some View {
         GeometryReader { geo in
             ScrollView{
                 VStack (alignment: .leading){
-                    ZStack {
-                        Image(recipe.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: geo.size.width, height: 280)
-                            .clipped()
-                    }
+                    Image(recipe.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geo.size.width, height: 280)
+                        .clipped()
+                    
+                    HStack {
+                        Text("Select your serving size: ")
+                        Picker("Picker", selection: $selectedIndex){
+                            ForEach(servingSelections, id: \.self){ servingSelection in
+                                Text(String(servingSelection)).tag(servingSelection)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .frame(width: 150)
+                    }.padding()
                     
                     VStack (alignment: .leading){
                         Text("Ingredients")
@@ -28,7 +39,7 @@ struct RecipeDetailView: View {
                             .bold()
                             .padding(.vertical , 5)
                         ForEach(recipe.ingredients) { ingredient in
-                            Text(" ·  " + ingredient.name)
+                            Text(" ·  \(ingredient.getPortion(recipeServings: recipe.servings, targetServings: selectedIndex)) \(ingredient.name)")
                                 .padding(.bottom , 1)
                         }
                         .padding(.horizontal, 5)
